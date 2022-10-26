a31551adc6b9f7aecc4061795f2403c968456f5a
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-27 01:21:48 +0900

    initial commit

--- getFollowers.py
+++ getFollowers.py
@@ -1,31 +1,39 @@
-CONSUMER_KEY = "******************"
-CONSUMER_SECRET = "******************"
-ACCESS_TOKEN = "******************"
-ACCESS_TOKEN_SECRET = "******************"
+CONSUMER_KEY = "1vJb727lQTjybAyQ4MqGYjss4"
+CONSUMER_SECRET = "SYshDqqjAVK5sgaPsfizHQmGheNnQxRP0s6GlMIb98imYPFvSE"
+ACCESS_TOKEN = "1482836345986826242-594q3iMdw8A6lnZsa157yoUTdyRzQX"
+ACCESS_TOKEN_SECRET = "tP9yxqAuGaAOgVadjJXqdFBj1dRDaE16AQLlhawx6PbTX"
 
 import tweepy
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth,wait_on_rate_limit = True)
 
-user = input("「@ユーザー名」を入力してください：")
-followerIDs = api.followers_ids(user)
+#user = input("「@ユーザー名」を入力してください：")
+#followerIDs = api.get_follower_ids(id="tech_comi")
+#print(len(followerIDs))
+followerIDs = []
+for followerID in tweepy.Cursor(api.get_follower_ids, id="tech_comi").items():
+    followerIDs.append(followerID)
 
+print(len(followerIDs))
+count = 0
 followerDatas = []
 for followerID in followerIDs:
     followerData = {}
-    data = api.get_user(followerID)
+    data = api.get_user(user_id=followerID)
     followerData["Name"] = data.name
+    followerData["ID"] = followerID
     followerData["Follow"] = data.friends_count
     followerData["Follower"] = data.followers_count
     followerData["Description"] = data.description
     followerData["TweetCount"] = data.statuses_count
     followerDatas.append(followerData)
-    print(followerData)
+    count +=1
+    print(count)
 
 import pandas as pd
 pd.set_option("display.max_rows", 1000)
-df = pd.DataFrame(followerDatas).loc[:,["Name","Follow","Follower","TweetCount","Description"]]
+df = pd.DataFrame(followerDatas).loc[:,["Name","ID","Follow","Follower","TweetCount","Description"]]
 
 #ファイル出力
 fileName = input("ファイル名を入力してください：")
